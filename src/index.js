6a6e6f86c23348f66f5245e682302d3ff585215f
Your Name <you@example.com>
2023-02-18 22:01:24 +0530

    fix some bug

--- src/index.js
+++ src/index.js
@@ -5,19 +5,22 @@ import App from './App';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDugTd6v79Oo9cNU4ds2PThRh2xbmGyz0o",
-  authDomain: "cart-1d285.firebaseapp.com",
-  projectId: "cart-1d285",
-  storageBucket: "cart-1d285.appspot.com",
-  messagingSenderId: "1081214064423",
-  appId: "1:1081214064423:web:c871f681503ae0d5c5fb02"
+  apiKey: `${process.env.REACT_APP_apiKey}`,
+  authDomain: `${process.env.REACT_APP_authDomain}`,
+  projectId: `${process.env.REACT_APP_projectId}`,
+  storageBucket: `${process.env.REACT_APP_storageBucket}`,
+  messagingSenderId: `${process.env.REACT_APP_messagingSenderId}`,
+  appId: `${process.env.REACT_APP_appId}`
 };
-
+console.log(`${process.env.REACT_APP_name}`)
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app); //create instance of firebase access and export it 
